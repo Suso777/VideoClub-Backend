1202d3bac065ea1f22e4e00714a80e16337f4470
Suso777 <xesussuarez@gmail.com>
2026-03-24 10:16:53 +0100

    Feat MoovieController.java

--- src/main/java/org/example/controller/MoovieController.java
+++ src/main/java/org/example/controller/MoovieController.java
@@ -1,4 +1,22 @@
-package org.example.controller;
+package org.videoclub.controller;
 
-public class MoovieController {
+import org.videoclub.model.Movie;
+import org.videoclub.repository.MovieRepositoryImpl;
+import java.util.List;
+
+public class MovieController {
+
+    private MovieRepositoryImpl movieRepository;
+
+    public MovieController(MovieRepositoryImpl movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public void createMovieController(Movie movie) {
+        movieRepository.createMovie(movie);
+    }
+
+    public List<Movie> findAllController() {
+        return movieRepository.findAll();
+    }
 }
