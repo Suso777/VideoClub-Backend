3ade6b2622a31a7590876a0d186cb987cea22b7c
Suso777 <xesussuarez@gmail.com>
2026-03-24 11:32:32 +0100

    Fix main and MovieView

---
+++ src/main/java/org/videoclub/Main.java
@@ -0,0 +1,17 @@
+package org.videoclub;
+
+import org.videoclub.controller.MovieController;
+import org.videoclub.repository.MovieRepositoryImpl;
+import org.videoclub.view.MovieView;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MovieRepositoryImpl movieRepository = new MovieRepositoryImpl();
+        MovieController movieController = new MovieController(movieRepository);
+        MovieView movieView = new MovieView(movieController);
+
+         movieView.createMovieView();
+        movieView.showMovies();
+    }
+}
