83196ed736d900db629ade979e26c3edd5bdf8c9
Suso777 <xesussuarez@gmail.com>
2026-03-24 10:14:50 +0100

    Feat DBManager.java

--- src/main/java/org/example/config/DBManager.java
+++ src/main/java/org/example/config/DBManager.java
@@ -1,4 +1,37 @@
-package org.example.config;
+package org.videoclub.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBManager {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/movies_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    public static Connection initConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexión a la BBDD exitosa");
+
+        } catch (SQLException exception) {
+            System.out.println("Conexión fallida: " + exception.getMessage());
+        }
+
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) { // Seguridad extra
+                connection.close();
+                System.out.println("Conexión cerrada con Éxito");
+            }
+        } catch (SQLException exception) {
+            System.out.println("Error al cerrar la BBDD: " + exception.getMessage());
+        }
+    }
 }
